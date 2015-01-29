130c5eb2f588c2043a638b98356abf53573d164e
Tanjai Kongyuen <tanjaik@wotifgroup.com>
2015-01-29 11:15:42 +0700

    Practice Invoking Apps

--- src/desiredcapab.java
+++ src/desiredcapab.java
@@ -1,8 +1,11 @@
 import static org.junit.Assert.*;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,7 +14,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class desiredcapab {
 
 	@Test
-	public void test() {
+	public void test() throws MalformedURLException {
 		       File appDir = new File("src");
 		
 		     File app = new File(appDir,"bookMyShow-ucb.apk");
@@ -20,6 +23,7 @@ public class desiredcapab {
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,MobilePlatform.ANDROID);
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
 		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		AndroidDriver driver = new AndroidDriver( new URL ("http://127.0.0.1:4723/wd/hub"),cap );
 		
 		
 		
